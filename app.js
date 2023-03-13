6acab080bf2509e5c062fbf9075bc8d480f2cf44
ChaseDev <chasewebdev101@gmail.com>
2023-03-13 15:18:54 -0700

    Integrate random color as option with grid

--- app.js
+++ app.js
@@ -2,83 +2,7 @@ const container = document.querySelector(".container");
 const btnColor = document.createElement("button");
 btnColor.style.backgroundColor = "black";
 
-// Selects the color black to be used when btnBlack is clicked.
-const btnBlack = document.querySelector(".btn-black");
-
-btnBlack.addEventListener("click", runBlackButton);
-
-function runBlackButton() {
-    btnColor.style.backgroundColor = "black";
-}
-
-// Selects the color yellow to be used when btnYellow is clicked.
-const btnYellow = document.querySelector(".btn-yellow");
-
-btnYellow.addEventListener("click", runYellowButton);
-
-function runYellowButton() {
-    btnColor.style.backgroundColor = "yellow";
-}
-
-// Selects the color blue to be used when btnBlue is clicked.
-const btnBlue = document.querySelector(".btn-blue");
-
-btnBlue.addEventListener("click", runBlueButton);
-
-function runBlueButton() {
-    btnColor.style.backgroundColor = "blue";
-}
-
-// Selects the color orange to be used when btnOrange is clicked.
-const btnOrange = document.querySelector(".btn-orange");
-
-btnOrange.addEventListener("click", runOrangeButton);
-
-function runOrangeButton() {
-    btnColor.style.backgroundColor = "orange";
-}
-
-// Selects the color green to be used when btnGreen is clicked.
-const btnGreen = document.querySelector(".btn-green");
-
-btnGreen.addEventListener("click", runGreenButton);
-
-function runGreenButton() {
-    btnColor.style.backgroundColor = "green";
-}
-
-// Selects the color red to be used when btnRed is clicked.
-const btnRed = document.querySelector(".btn-red");
-
-btnRed.addEventListener("click", runRedButton);
-
-function runRedButton() {
-    btnColor.style.backgroundColor = "red";
-}
-
-// Selects the color white to erase other colors with btnEraser is clicked.
-const btnEraser = document.querySelector(".btn-eraser");
-
-btnEraser.addEventListener("click", runEraserButton);
-
-function runEraserButton() {
-    btnColor.style.backgroundColor = "white";
-}
-
-// Creates a default 16x16 grid when the page loads
-for (i = 0; i < 256; i++) {
-    const div = document.createElement("button");
-    div.classList.add("btn-grid");
-    div.style.height = "32px";
-    div.style.width = "32px";
-    div.style.margin = "0px";
-    div.style.padding = "0px";
-    div.style.borderRadius = "0px";
-    div.style.border = "0px";
-    div.textContent = "";
-    div.style.backgroundColor = "white";
-    container.appendChild(div);
-}
+smallGrid();
 
 const btnSmall = document.querySelector(".btn-small");
 
@@ -109,6 +33,139 @@ function smallGrid () {
         button.style.backgroundColor = btnColor.style.backgroundColor;
         });
     });
+
+    // Selects the color black to be used when btnBlack is clicked.
+    const btnBlack = document.querySelector(".btn-black");
+
+    btnBlack.addEventListener("click", runBlackButton);
+
+    function runBlackButton() {
+        btnColor.style.backgroundColor = "black";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color yellow to be used when btnYellow is clicked.
+    const btnYellow = document.querySelector(".btn-yellow");
+
+    btnYellow.addEventListener("click", runYellowButton);
+
+    function runYellowButton() {
+        btnColor.style.backgroundColor = "yellow";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color blue to be used when btnBlue is clicked.
+    const btnBlue = document.querySelector(".btn-blue");
+
+    btnBlue.addEventListener("click", runBlueButton);
+
+    function runBlueButton() {
+        btnColor.style.backgroundColor = "blue";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color orange to be used when btnOrange is clicked.
+    const btnOrange = document.querySelector(".btn-orange");
+
+    btnOrange.addEventListener("click", runOrangeButton);
+
+    function runOrangeButton() {
+        btnColor.style.backgroundColor = "orange";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color green to be used when btnGreen is clicked.
+    const btnGreen = document.querySelector(".btn-green");
+
+    btnGreen.addEventListener("click", runGreenButton);
+
+    function runGreenButton() {
+        btnColor.style.backgroundColor = "green";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color red to be used when btnRed is clicked.
+    const btnRed = document.querySelector(".btn-red");
+
+    btnRed.addEventListener("click", runRedButton);
+
+    function runRedButton() {
+        btnColor.style.backgroundColor = "red";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color white to erase other colors with btnEraser is clicked.
+    const btnEraser = document.querySelector(".btn-eraser");
+
+    btnEraser.addEventListener("click", runEraserButton);
+
+    function runEraserButton() {
+        btnColor.style.backgroundColor = "white";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Generate a random color HEX number.
+    function generateRandomColor () {
+        // Generates a random HEX color.
+        let numberMax = 0xFFFFFF;
+        let randomNumber = Math.random() * numberMax;
+        randomNumber = Math.floor(randomNumber);
+        randomNumber = randomNumber.toString(16);
+        let randomColor = randomNumber.padStart(6, 0);
+        return `#${randomColor.toUpperCase()}`;
+    }
+
+    // Selects a random number to assign a color to be used when btnRandom is clicked.
+    const btnRandom = document.querySelector(".btn-random");
+
+    btnRandom.addEventListener("click", runRandomColorButton);
+
+    // Generates random color to be used in the grid.
+    function runRandomColorButton () {
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = generateRandomColor();
+            });
+        });
+    }
+
+    btnRandom.removeEventListener;
 }
 
 const btnMedium = document.querySelector(".btn-medium");
@@ -140,6 +197,139 @@ function mediumGrid () {
         button.style.backgroundColor = btnColor.style.backgroundColor;
         });
     });
+
+    // Selects the color black to be used when btnBlack is clicked.
+    const btnBlack = document.querySelector(".btn-black");
+
+    btnBlack.addEventListener("click", runBlackButton);
+
+    function runBlackButton() {
+        btnColor.style.backgroundColor = "black";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color yellow to be used when btnYellow is clicked.
+    const btnYellow = document.querySelector(".btn-yellow");
+
+    btnYellow.addEventListener("click", runYellowButton);
+
+    function runYellowButton() {
+        btnColor.style.backgroundColor = "yellow";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color blue to be used when btnBlue is clicked.
+    const btnBlue = document.querySelector(".btn-blue");
+
+    btnBlue.addEventListener("click", runBlueButton);
+
+    function runBlueButton() {
+        btnColor.style.backgroundColor = "blue";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color orange to be used when btnOrange is clicked.
+    const btnOrange = document.querySelector(".btn-orange");
+
+    btnOrange.addEventListener("click", runOrangeButton);
+
+    function runOrangeButton() {
+        btnColor.style.backgroundColor = "orange";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color green to be used when btnGreen is clicked.
+    const btnGreen = document.querySelector(".btn-green");
+
+    btnGreen.addEventListener("click", runGreenButton);
+
+    function runGreenButton() {
+        btnColor.style.backgroundColor = "green";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color red to be used when btnRed is clicked.
+    const btnRed = document.querySelector(".btn-red");
+
+    btnRed.addEventListener("click", runRedButton);
+
+    function runRedButton() {
+        btnColor.style.backgroundColor = "red";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color white to erase other colors with btnEraser is clicked.
+    const btnEraser = document.querySelector(".btn-eraser");
+
+    btnEraser.addEventListener("click", runEraserButton);
+
+    function runEraserButton() {
+        btnColor.style.backgroundColor = "white";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Generate a random color HEX number.
+    function generateRandomColor () {
+        // Generates a random HEX color.
+        let numberMax = 0xFFFFFF;
+        let randomNumber = Math.random() * numberMax;
+        randomNumber = Math.floor(randomNumber);
+        randomNumber = randomNumber.toString(16);
+        let randomColor = randomNumber.padStart(6, 0);
+        return `#${randomColor.toUpperCase()}`;
+    }
+
+    // Selects a random number to assign a color to be used when btnRandom is clicked.
+    const btnRandom = document.querySelector(".btn-random");
+
+    btnRandom.addEventListener("click", runRandomColorButton);
+
+    // Generates random color to be used in the grid.
+    function runRandomColorButton () {
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = generateRandomColor();
+            });
+        });
+    }
+
+    btnRandom.removeEventListener;
 }
 
 const btnLarge = document.querySelector(".btn-large");
@@ -171,13 +361,137 @@ function largeGrid () {
         button.style.backgroundColor = btnColor.style.backgroundColor;
         });
     });
+
+    // Selects the color black to be used when btnBlack is clicked.
+    const btnBlack = document.querySelector(".btn-black");
+
+    btnBlack.addEventListener("click", runBlackButton);
+
+    function runBlackButton() {
+        btnColor.style.backgroundColor = "black";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color yellow to be used when btnYellow is clicked.
+    const btnYellow = document.querySelector(".btn-yellow");
+
+    btnYellow.addEventListener("click", runYellowButton);
+
+    function runYellowButton() {
+        btnColor.style.backgroundColor = "yellow";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color blue to be used when btnBlue is clicked.
+    const btnBlue = document.querySelector(".btn-blue");
+
+    btnBlue.addEventListener("click", runBlueButton);
+
+    function runBlueButton() {
+        btnColor.style.backgroundColor = "blue";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color orange to be used when btnOrange is clicked.
+    const btnOrange = document.querySelector(".btn-orange");
+
+    btnOrange.addEventListener("click", runOrangeButton);
+
+    function runOrangeButton() {
+        btnColor.style.backgroundColor = "orange";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color green to be used when btnGreen is clicked.
+    const btnGreen = document.querySelector(".btn-green");
+
+    btnGreen.addEventListener("click", runGreenButton);
+
+    function runGreenButton() {
+        btnColor.style.backgroundColor = "green";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color red to be used when btnRed is clicked.
+    const btnRed = document.querySelector(".btn-red");
+
+    btnRed.addEventListener("click", runRedButton);
+
+    function runRedButton() {
+        btnColor.style.backgroundColor = "red";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Selects the color white to erase other colors with btnEraser is clicked.
+    const btnEraser = document.querySelector(".btn-eraser");
+
+    btnEraser.addEventListener("click", runEraserButton);
+
+    function runEraserButton() {
+        btnColor.style.backgroundColor = "white";
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = btnColor.style.backgroundColor;
+            });
+        });
+    }
+
+    // Generate a random color HEX number.
+    function generateRandomColor () {
+        // Generates a random HEX color.
+        let numberMax = 0xFFFFFF;
+        let randomNumber = Math.random() * numberMax;
+        randomNumber = Math.floor(randomNumber);
+        randomNumber = randomNumber.toString(16);
+        let randomColor = randomNumber.padStart(6, 0);
+        return `#${randomColor.toUpperCase()}`;
+    }
+
+    // Selects a random number to assign a color to be used when btnRandom is clicked.
+    const btnRandom = document.querySelector(".btn-random");
+
+    btnRandom.addEventListener("click", runRandomColorButton);
+
+    // Generates random color to be used in the grid.
+    function runRandomColorButton () {
+        // Changes the color of the grid when clicked, and uses whichever color is selected.
+        buttons.forEach((button) => {
+            button.addEventListener("click", () => {
+                button.style.backgroundColor = generateRandomColor();
+            });
+        });
+    }
+
+    btnRandom.removeEventListener;
 }
-
-const buttons = document.querySelectorAll(".btn-grid");
-
-// Changes the color of the grid when clicked, and uses whichever color is selected.
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        button.style.backgroundColor = btnColor.style.backgroundColor;
-    })
-})
